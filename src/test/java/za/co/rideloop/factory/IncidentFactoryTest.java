8ba370d8504e5434c3450812c804f5e342989c16
RatiaSB <230724477@mycput.ac.za>
2025-05-17 21:40:21 +0200

    Added factory class and factory test for incident

---
+++ src/test/java/za/co/rideloop/factory/IncidentFactoryTest.java
@@ -0,0 +1,55 @@
+package za.co.rideloop.factory;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import za.co.rideloop.Domain.Incident;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * IncidentFactoryTest.java
+ * IncidentFactoryTest model class
+ *
+ * @author : Swatsi Bongani Ratia
+ * @studnr : 230724477
+ * @group : 3I
+ * @Java version: "21.0.3" 2024-04-16 LTS
+ */
+
+public class IncidentFactoryTest {
+    @Test
+    void testBuildSuccess() {
+        Incident incident = IncidentFactory.build(
+                1,
+                "Accident",
+                "Car was rear-ended at signal."
+        );
+
+        assertNotNull(incident);
+        assertEquals(1, incident.getIncidentID());
+        assertEquals("Accident", incident.getIncidentType());
+        assertEquals("Car was rear-ended at signal.", incident.getDescription());
+    }
+
+    @Test
+    void testBuildWithEmptyIncidentType_shouldThrowException() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            IncidentFactory.build(2, "", "Flat tyre on highway.");
+        });
+
+        String expectedMessage = "Invalid input";
+        assertTrue(exception.getMessage().contains(expectedMessage));
+    }
+
+    @Test
+    void testBuildWithNullDescription_shouldThrowException() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            IncidentFactory.build(3, "Mechanical Failure", null);
+        });
+
+        String expectedMessage = "Invalid input";
+        assertTrue(exception.getMessage().contains(expectedMessage));
+    }
+
+}
