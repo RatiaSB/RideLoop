9218cd326e0674e36a28709086979b5cdbe319a8
RatiaSB <230724477@mycput.ac.za>
2025-05-17 21:59:17 +0200

    Created factory class for SecurityCompany

---
+++ src/main/java/za/co/rideloop/factory/SecurityCompanyFactory.java
@@ -0,0 +1,13 @@
+package za.co.rideloop.factory;
+
+/**
+*SecurityCompanyFactory.java
+*SecurityCompanyFactory model class
+*@author : Swatsi Bongani Ratia
+*@studnr : 230724477
+*@group  : 3I
+*@Java version: "21.0.3" 2024-04-16 LTS
+*/
+
+public class SecurityCompanyFactory {
+}
